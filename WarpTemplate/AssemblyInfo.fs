470c779d63db3ed5f0cbfc09970729ee65f07380
Kimserey <Kimserey@users.noreply.github.com>
2016-03-18 01:35:06 +0000

    Update AssemblyInfo.fs

--- WarpTemplate/AssemblyInfo.fs
+++ WarpTemplate/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace SequenceIssue.AssemblyInfo
+﻿namespace WarpTemplate.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("SequenceIssue")>]
+[<assembly: AssemblyTitle("WarpTemplate")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("SequenceIssue")>]
+[<assembly: AssemblyProduct("WarpTemplate")>]
 [<assembly: AssemblyCopyright("Copyright ©  2016")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
